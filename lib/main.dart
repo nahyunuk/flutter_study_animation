61a3dda76c768161eb5db36b98aa66003ebc521c
nahyunuk <s24022@gsm.hs.kr>
2024-09-26 21:01:36 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:study_animation/AnimatedContainerPage.dart';
+import 'package:study_animation/AnimatedDefaultTextStylePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AnimatedContainerPage(),
+      home: AnimatedDefaultTextStylePage(),
     );
   }
 }
