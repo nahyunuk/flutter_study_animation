a8cdf24412e78431183536eee6a84e7d0ec10012
nahyunuk <s24022@gsm.hs.kr>
2024-09-28 08:20:30 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:study_animation/ControllerWithCurvedAnimation.dart';
+import 'package:study_animation/ControllerWithRotationAnimation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ControllerWithCurvedAnimation(),
+      home: ControllerWithRotationAnimation(),
     );
   }
 }
